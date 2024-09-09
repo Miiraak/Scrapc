e6fb9752bcd8f7db74e6ad6a3693fd1e6358a545
Miiraak <miiraak@feynlok.ch>
2024-09-10 01:33:11 +0200

    Refactor MessageBox to use CheckedListBox for URLs
    - Replaced RichTextBox with CheckedListBox in MessageBox.Designer.cs to display URLs.
    - Added three new buttons: buttonOK, button1, and
    buttonUnselect for confirming selection, selecting all items, and
    unselecting all items, respectively.
    - Updated MessageBox.cs to populate CheckedListBox with URLs and
    handle click events for the new buttons.
    - Implemented buttonOK_Click to update the list of URLs based on checked items and close the form, buttonSelect_Click to select all items, and buttonUnselect_Click to unselect all items.
    - Cleaned up MessageBox.resx by removing unnecessary whitespace and comments for better readability.

--- Scrapc/MessageBox.cs
+++ Scrapc/MessageBox.cs
@@ -14,7 +14,37 @@
         private void ShowURLs()
         {
             // Display all the URLs gathered into the richTextBoxListURL
-            richTextBoxListURL.Text = string.Join("\n", MenuInfo.AllUrls);
+            foreach (var url in MenuInfo.AllUrls)
+            {
+                checkedListBoxURL.Items.Add(url);
+            }
+
+            for (int i = 0; i < checkedListBoxURL.Items.Count; i++)
+            {
+                checkedListBoxURL.SetItemChecked(i, true);
+            }
+        }
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            MenuInfo.AllUrls.Clear();
+            MenuInfo.AllUrls = checkedListBoxURL.CheckedItems.Cast<string>().ToList();
+            this.Close();
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBoxURL.Items.Count; i++)
+            {
+                checkedListBoxURL.SetItemChecked(i, true);
+            }
+        }
+
+        private void buttonUnselect_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBoxURL.Items.Count; i++)
+            {
+                checkedListBoxURL.SetItemChecked(i, false);
+            }
         }
     }
 }
